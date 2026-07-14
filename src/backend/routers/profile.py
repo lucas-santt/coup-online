655bc5b1a66522aa96391b7d2ad5325ea092e0c3
pedrobgennari <pedro.pbg@usp.br>
2026-07-13 22:36:04 -0300

    feat(backend::api): add base profile endpoint
    This adds the profile endpoints for getting a player's profile and changing the display name. It also includes tests and changes some dependencies.
    
    Known issues:
    - Pytest ends up cluttering 'assets/profile_pictures' with files. This will (?) be fixed later by changing the "PROFILE_PICTURES_DIR" constant into a "upload_dir" setting and using the "tmp_path" fixture.

--- src/backend/routers/profile.py
+++ src/backend/routers/profile.py
@@ -1,18 +1,55 @@
-from fastapi import APIRouter, Depends, HTTPException
+from typing import Annotated
+import shutil
+
+from fastapi import APIRouter, Body, File, UploadFile
+
+from backend.auth.required import RequiredRegisteredOrGuestDep
+from backend.constants import PROFILE_PICTURES_DIR
+from backend.database import SessionDep, add_to_db
 
 router = APIRouter(prefix="/api/profile", tags=["profile"])
 
 
 @router.get("/me")
-async def me():
-    pass
+async def me(player: RequiredRegisteredOrGuestDep) -> dict[str, str | bool | None]:
+    return {
+        "username": player.username,
+        "displayname": player.displayname,
+        "avatar_url": player.avatar_url,
+        "is_guest": player.is_guest,
+    }
 
 
-@router.get("/display-name")
-async def display_name():
-    pass
+@router.patch("/display-name")
+async def set_display_name(
+    displayname: Annotated[str, Body(embed=True)],
+    session: SessionDep,
+    player: RequiredRegisteredOrGuestDep,
+) -> dict[str, str | None]:
+    # TODO?: Validate display name
+
+    player.displayname = displayname
+    add_to_db(player, session)
+    return {"displayname": player.displayname}
 
 
-@router.get("/avatar")
-async def avatar():
-    pass
+@router.patch("/avatar")
+async def set_avatar(
+    avatar: Annotated[UploadFile, File()],
+    session: SessionDep,
+    player: RequiredRegisteredOrGuestDep,
+) -> dict[str, str]:
+    # TODO: Validate/Process file
+
+    # TODO: Make this use settings.upload_dir instead of a constant so that
+    #  pytest can override it and use tmp_dir instead
+
+    avatar_url = PROFILE_PICTURES_DIR / f"{player.id.hex}.png"
+
+    with open(avatar_url.resolve(), "wb+") as file:
+        shutil.copyfileobj(avatar.file, file)
+
+    player.avatar_url = str(avatar_url)
+    add_to_db(player, session)
+
+    return {"avatar_url": player.avatar_url}
